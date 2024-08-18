369e4de223d953e350d5166ecfc3543e8519f121
Faocci Layug <your_email@example.com>
2024-08-18 11:07:23 +1000

    Change name to FGOL

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = () => {
             <Link href="#">
               <div className="flex items-center space-x-3 cursor-pointer">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap">
-                  FGL
+                  FGOL
                 </span>
               </div>
             </Link>
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,10 +4,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "FGL",
+  title: "FGOL",
   description:
     "Hello, I'm Faocci Ghaize O. Layug, a tech and design enthusiast ready to bring my expertise to your projects. With a knack for creative problem-solving in web and graphic design, and a solid foundation in computer technology, I'm excited to contribute to a team that values innovation and excellence. Let's make a positive impact in the digital sphere together!",
-  keywords: "Faocci Layug, FGL, Faocci, Ghaize, BSIT",
+  keywords: "Faocci Layug, FGOL, Faocci, Ghaize, BSIT",
   author: "Faocci Ghaize O. Layug",
   robots: "Faocci, Layug",
 };
